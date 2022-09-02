b5ea7dac2e95d900703c329cd5094447c4bfce5b
codebotx <anubhabr50@gmail.com>
2022-09-02 11:16:28 +0530

    Merge Two Sorted Lists.cpp

--- 27. Merge Two Sorted Lists.cpp
+++ 27. Merge Two Sorted Lists.cpp
@@ -1,22 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        ListNode* res=list1;
-        ListNode* t1=list1;
-        ListNode* t2= list2;
-        while(t1->next!=nullptr && t2->next!=nullptr){
-            if(t1->val>t2->val){
-                res->val=t1->val;
-                res->next=nullptr;
-                res=res->next;
-                t1=t1->next;
+        if(list1==NULL)
+            return list2;
+        if(list2==NULL)
+            return list1;
+        if(list1->val > list2->val) std::swap(list1,list2);
+        ListNode *res= list1;
+        while(list1!=NULL && list2!=NULL){
+            ListNode* temp=NULL;
+            while(list1!=NULL && list1->val<=list2->val){
+                temp=list1;
+                list1=list1->next;
             }
-            else{
-                res->val=t1->val;
-                res->next=nullptr;
-                res=res->next;
-                t1=t1->next;
-            }
+            temp->next = list2;
+            std::swap(list1,list2);
         }
         return res;
     }
