4760bf80acf00c4c2c4b37e9acdc3b0f1c2ee7ca
codebotx <anubhabr50@gmail.com>
2022-09-07 21:32:09 +0530

    3sum-fin

--- 39. 3sum.cpp
+++ 39. 3sum.cpp
@@ -37,5 +37,35 @@ public:
 		//         return res;
 		//     }
 		// }
+		sort(nums.begin(), nums.end());
+		set<vector<int>> s;
+		int check = INT_MIN;
+		for (auto it = nums.begin(); it != nums.end(); it++)
+		{
+			if (*it != check)
+			{
+				check = *it;
+				auto itl = it + 1;
+				auto itr = nums.end() - 1;
+				while (itr > itl)
+				{
+					if (*it + *itl + *itr == 0)
+					{
+						vector<int> v;
+						v.push_back(*it);
+						v.push_back(*itl);
+						v.push_back(*itr);
+						s.insert(v);
+						itr--;
+					}
+					else if (*it + *itl + *itr > 0)
+						itr--;
+					else if (*it + *itl + *itr < 0)
+						itl++;
+				}
+			}
+		}
+		vector<vector<int>> res(s.begin(), s.end());
+		return res;
 	}
 };
